4ddded02712138e7c235cdb83e31592e6a3774e6
luisa-charles <ljcharles@hotmail.co.uk>
2018-06-09 19:50:02 +0100

    trying to fix

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ app.use('/api', router);
 
 app.use(errorHandler);
 
-app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function (err) {
+        if (err) {
+            res.status(500).send(err)
+        }
+    })
+})
 
 app.listen(port, () => console.log(`Live on port ${port}`));
 
