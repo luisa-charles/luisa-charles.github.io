285db7115066084dfd4117c3462c1fe24becd497
luisa-charles <ljcharles@hotmail.co.uk>
2018-06-09 19:55:43 +0100

    fixing

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/api', router);
 app.use(errorHandler);
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function (err) {
+    res.sendFile(path.join(__dirname, '/index.html'), function (err) {
         if (err) {
             res.status(500).send(err)
         }
