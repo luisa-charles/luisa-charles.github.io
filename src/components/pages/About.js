0c7f16321684c80804e2e8e64aa7bb40529ebe70
luisa-charles <ljcharles@hotmail.co.uk>
2018-06-16 13:33:26 +0100

    added info to about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,12 +6,14 @@ const About = () => {
             <h1 className="title">About</h1>
             <div className="columns">
             <div className="column">
-            <img src={"http://placecorgi.com/250"} alt={"placeholder"} />
+                <img src={"https://i.imgur.com/OX8tJy3.jpg"} alt={"Picture of Luisa"} />
             </div>
             <div className="column">
-                <p>Lorem Khaled Ipsum is a major key to success. Another one. The key to more success is to have a lot of pillows. To be successful you’ve got to work hard, to make history, simple, you’ve got to make it. They will try to close the door on you, just open it. Bless up. Every chance I get, I water the plants, Lion! Major key, don’t fall for the trap, stay focused. It’s the ones closest to you that want to see you fail. They don’t want us to win. Special cloth alert.</p>
+                <p>Luisa Charles is a legend.</p>
                 <br />
-                <p>The weather is amazing, walk with me through the pathway of more success. Take this journey with me, Lion! Every chance I get, I water the plants, Lion! You do know, you do know that they don’t want you to have lunch. I’m keeping it real with you, so what you going do is have lunch. Major key, don’t fall for the trap, stay focused. It’s the ones closest to you that want to see you fail.</p>
+                <p>That is all.</p>
+                <br />
+                <p>Made with ♥︎ by Helena.</p>
             </div>
             </div>
         </main>
