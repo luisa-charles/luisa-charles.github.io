2356beead22943476b59e8d8c2788f1ad96c6750
luisa-charles <ljcharles@hotmail.co.uk>
2018-06-18 15:48:02 +0100

    styling video on project show

--- src/components/pages/ProjectsShow.js
+++ src/components/pages/ProjectsShow.js
@@ -31,7 +31,7 @@ class ProjectsShow extends React.Component {
                         <figure className="image projects-show-padding-bottom">
                             <img src={this.state.project.image} alt={this.state.project.name} />
                         </figure>
-                        <div style={{padding: '56.25% 0 0 0', position: 'relative'}}><iframe src={this.state.project.videoSrc} style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%'}} frameBorder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowFullScreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+                        <div style={{ padding: '56.25% 0 24px 0', position: 'relative' }}><iframe className="projects-show-padding-bottom" src={this.state.project.videoSrc} style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%'}} frameBorder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowFullScreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
                         <figure className="image projects-show-padding-bottom">
                             <img src={this.state.project.image} alt={this.state.project.name} />
                         </figure>
