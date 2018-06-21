607ec296e847e56d01a4e12ec689be60fac40955
luisa-charles <ljcharles@hotmail.co.uk>
2018-06-21 02:33:11 +0100

    trying to flex about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,24 +2,24 @@ import React from 'react';
 
 const About = () => {
     return (
-        <main>
+        <main id="about">
             <h1 className="title">About</h1>
-            <div className="columns">
+            {/* <div className="columns"> */}
             {/* <div> */}
-            <div className="column">
-            {/* <div> */}
-                <img src={"https://i.imgur.com/OX8tJy3.jpg"} alt={"Luisa Charles"} />
+            {/* <div className="column"> */}
+            <div className="flex-about">
+                <img className="about-photo" src={"https://i.imgur.com/OX8tJy3.jpg"} alt={"Luisa Charles"} />
             </div>
-            <div className="column">
-            {/* <div> */}
+            {/* <div className="column"> */}
+            <div className="flex-about">
                 <p>Luisa Charles is a recent graduate of BA (Hons) Interaction Design Arts from University of the Arts London, London College of Communication. She works across a wide range of media, from physical computing based interactive installations, to directing and designing for film and theatre. Much of her work comes under the umbrella of critical and speculative design, as well as applied work for gallery and private commissions.</p>
+            </div>
+            {/* </div> */}
                 <ul>
-                    <li>07772714511</li>
-                    <li><a href="emailto: ljcharles@hotmail.co.uk">Email Me</a></li>
-                    <li><a href="https://www.linkedin.com/Luisa-Charles">Find me on LinkedIn</a></li>
+                    <li className="contact-info">07772714511</li>
+                    <li className="contact-info"><a href="emailto: ljcharles@hotmail.co.uk">Email Me</a></li>
+                    <li className="contact-info"><a href="https://www.linkedin.com/Luisa-Charles">Find me on LinkedIn</a></li>
                 </ul>
-            </div>
-            </div>
         </main>
     );
 };
